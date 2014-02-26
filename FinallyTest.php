f0b7dccbaba82e4b688c6b728d278b58c54abcdd
Cyrus Boadway <cyrus@boadway.ca>
2014-02-26 09:14:00 -0500

    Update FinallyTest.php

--- FinallyTest.php
+++ FinallyTest.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Test's how PHP handles return statements
+ * Tests how PHP handles return statements
  * when a try-finally is used.
  *
  * The output of this is:
  * test 1: Inside try
  * test 1: Inside finally, a=2
- * test 1 returned: 1
+ * test 1: returned 2
  * test 2: Inside try
  * test 2: Inside finally, a=2
- * Test 2 returned: 2
+ * test 2: returned 2
  * test 3: Inside try, return 1 + 0
  * test 3: Inside finally, a=2
  * test 3: returned 1
